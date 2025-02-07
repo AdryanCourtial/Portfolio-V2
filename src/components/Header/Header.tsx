7c2b512d8b210d4309fcfa41a6e5d73620025da6
sebastien Dunod <sebastien.dunod@pme-partner.fr>
2025-02-07 19:09:57 +0100

    fin de la premiere page

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="fixed z-50 flex flex-row w-screen h-[4rem] bg-black-light text-white justify-around"> 
+            <button> Qui suis-je ? </button>
+            <button> Mes Competences </button>
+            <button> Mes Projects </button>
+            <button> Me Contacter </button> 
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,46 @@
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className="h-screen w-screen bg-grey-light">
+            <div>
+                <p className="absolute font-medium text-black-light top-[20%] italic left-[15%] titre_poste animation_slide_to_right"> 
+                    Developer Full Stack
+                </p>
+            </div>
+            <div className="absolute left-0 bottom-0 w-[80%] aspect-auto banner animation_slide_to_right">
+                <p className="absolute top-[50%] w-[40%] left-[10%] text-white -translate-y-[70%] animation_slide_to_right">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis delectus vel autem a, eum obcaecati totam inventore ducimus excepturi fuga reiciendis voluptatem facere recusandae. Optio quaerat obcaecati a similique fugit! 
+                </p>
+                <img src="/PresentationBanner.png" className="w-full h-full" />
+            </div>
+            <div className="relative left-0 top-[2%] w-[15%] min-w-[175px] page_number-1 animation_slide_to_right">
+                <img src="/page-1.png" className="h-[100%] w-auto aspect-auto" />
+            </div>
+            <div className="absolute top-[50%] right-0 w-[10%] aspect-auto -translate-y-[50%] ">
+                <img src="/Polygon1.png" />
+            </div>
+                
+            <div>
+                <p className="absolute font-extrabold text-black-light top-[10%] italic right-[8%] title_name animation_slide_to_right"> 
+                    ADRYAN <br /> COURTIAL
+                </p>
+            </div>
+            <div className="absolute bg-black-dark rounded-[10px] border-blue-main border-[5px] text-white px-[1.2rem] py-2 w-[15%] flex flex-row justify-center left-[5%] top-[50%] cursor-pointer animation_exitation min-w-[200px]">
+                <a className="w-full h-full text-center">
+                    Contacter-moi !
+                </a>
+            </div>
+            <div className="">
+                <div className="absolute -z-40 left-0 bottom-[2%] aspect-auto w-[35%] animation_font">
+                    <img src="/Rectangle4.png" />
+                </div>
+                <div className="absolute -z-40 aspect-auto w-[40%] right-50 bottom-0 animation_font" >
+                    <img src="/Rectangle5.png" />    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
